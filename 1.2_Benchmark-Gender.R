597b523479c5f484f958ca49ac40e56edad584a6
Roma <romanofkin@gmail.com>
2017-12-29 14:04:45 -0800

    Benchmark based on all Females surviving

---
+++ 1.2_Benchmark-Gender.R
@@ -0,0 +1,18 @@
+# Benchmark submission: All Females Survived
+
+# Load packages
+library(readr)
+library(dplyr)
+
+# Read in the Test data
+titanic <- read_csv("Data/titanic_test.csv")
+titanic
+
+benchmark_df <- titanic %>%
+  select(PassengerId, Sex) %>%
+  mutate(Survived = ifelse(Sex == "female", 1, 0)) %>%
+  select(PassengerId, Survived) 
+
+benchmark_df
+
+write_csv(benchmark_df, "Data/Benchmark-Gender.csv")
